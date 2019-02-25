f6feff4dc302ec8b48b277491ba70c4fb6091c6b
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-02-25 11:47:53 +0200

    None disabled for name

--- pypass.py
+++ pypass.py
@@ -1,17 +1,18 @@
 import datetime
 
 name = str(input(">>>Name: "))
-if name == 0:
-    print("You have to enter at least the name")
+if name == "":
+    name = str(input("You have to enter at least the name: "))
+
 
 surname = str(input(">>>Surname: "))
 nickname = str(input(">>>Nickname: "))
 dob = str(input(">>>Date of Birth DDMMYYYY: "))
     
 adds = str(input("\n\nWould you like to add more information about the target?[Y/N]: "))
-if adds == 'Y':
+if adds == 'y' or 'Y':
     other = str(input("Things like fav series/band, food or phrases separated by commas: "))
-elif adds == 'N':
+elif adds == 'N' or 'n':
     print('Password list ready!')
 else :
     print("Invalid input!")
