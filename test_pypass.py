79bae814fbcf49adb5e02e444dcc1c49daa4b02e
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-02-25 14:47:32 +0200

    writing tests

--- test_pypass.py
+++ test_pypass.py
@@ -1,2 +1,9 @@
 import pytest
+from pypass import clean_input
 
+def test_clean():
+    assert clean_input('bob') == 'bob'
+    assert clean_input('Mary-Jane') == 'maryjane'
+    assert clean_input('#$%tsitsi&') == 'tsitsi'
+    assert clean_input(' sarah ') == 'sarah'
+    assert clean_input('JOHN') == 'john'
