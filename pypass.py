015a0a9bbfe1d44b036058c701d121cc12833cc9
bevennyamande <bevennyamande@gmail.com>
2019-02-27 01:36:10 +0200

    add password.txt

--- pypass.py
+++ pypass.py
@@ -1,33 +1,30 @@
-import datetime
 import click
+
+
+def other_info_callback(ctx, param, value):
+    pass
 
 
 @click.command()
 @click.option('--fullname', prompt='Enter target\'s fullname',
-              help='Target\'s fullname ',
-              type=click.STRING)
+              help='Target\'s fullname ')
 @click.option('--nickname', prompt='Enter target\'s nickname',
               help='Target\'s nickname')
 @click.option('--date_of_birth',
-              prompt='Enter target\'s date of birth in this format DDMMYYYY',
-              help='Target\'s date of birth')
+              prompt='Enter target\'s date of birth in this format dd-mm-yyyy',
+              help='Target\'s date of birth',
+              type=click.DateTime(formats=['%d-%m-%Y']))
 @click.option('--other',
-              prompt="Do you have other info about the target? If 'yes'"
-              "provide them. If 'not' just type 'no' ",
-              help='Other useful info about the target ie hobbies, likes, etc')
+              prompt="Do you have other info about the target? ",
+              help='Other useful info about the target ie hobbies, likes, etc',
+              type=click.Choice(['yes', 'no']), callback=other_info_callback)
 @click.option('--output_file',
               help='The file to write information about the target',
-              default='reconnoisance.txt')
+              default='password.txt', show_default=True)
 def pypass(fullname, nickname, date_of_birth, other, output_file):
     '''
     Command line password cracker based on user provided information
     '''
-
-    try:
-        # validate date of birth here
-        date_of_birth = datetime.datetime.strptime(date_of_birth, '%d%m%Y')
-    except Exception:
-        click.echo("Incorrect date of birth format, date should be DDMMYYYY")
 
     if fullname and nickname and date_of_birth:
         # if all the information about the target has been provided
