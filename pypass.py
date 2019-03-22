c95a9bb5a0d6eaeef6dd6d08f9e5b76ae5c39222
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-03-22 12:07:18 +0200

    CLI options now working

--- pypass.py
+++ pypass.py
@@ -17,31 +17,29 @@ def takeother(other):
     return other.split(',')
 
 
+   
+
 @click.command()
-def main():
-    click.echo("Welcome to pypass v0.1.                         ========
-                                                                |
-    ")
-
-
-
-
-
+@click.option('--about', is_flag=True, help='Information about the program and its usage.')
 #interactive shell, accepting input from the user
-'''@click.command()
-def main():
+def main(about):
+    profile = {}
+    if about:
+        click.echo("Welcome to pypass Version0.1!      pypass --help for more options")
     name = click.prompt('>>>Enter name ', type=str)
     surname = click.prompt('>>>Enter surname ', type=str)
     nickname = click.prompt('>>>Enter nickname ', type=str)
-    dob = click.prompt('>>>Enter date of birth ', type=datetime.date)
-    if click.confirm('>>>Any additional info ', default=True, abort=True):
-        other = click.prompt('>>>Hobbies, favourites and obssessions ', type=list)
-        click.echo('Password list ready!')
 
-    return(name, surname, nickname, dob, other)
-'''
+    profile['name'] = name
+    profile['surname'] = surname
+    profile['nickname'] = nickname
 
-if __name__ == '__main__':
+    print(profile)
+
+
+if __name__ == "__main__":
     main()
+
+
 
 
