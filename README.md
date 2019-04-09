e544d15beaa581b089c498b5ba063912f505439e
tsitsi-flora <tsitsimunikwa97@gmail.com>
2019-04-09 11:13:31 +0200

    usable now

--- README.md
+++ README.md
@@ -6,7 +6,13 @@
 
 Weak passwords can be defined as short, those that use only alphanumeric characters or one that is "obvious". When a password is obvious it means it's easy to remember for that person, but also easy to decrypt for an attacker.People often use obvious passwords such as the names of their children or their house number in order not to forget them. However, the simpler the password, the easier to detect. Because people tend to create "obvious" passwords more, I created pypass, a program that returns a list of passwords based on the target information.
 
-pypass is a password compiler, written in Python. It accepts input about the target from the user and returns possible passwords in a .txt file. Password lists are custom made, based on the information the user provides. This caters for passwords that include native language words. pypass can be used when performing password cracking(ethically) using the dictionary attack.
+pypass is a command line password compiler, written in Python. It accepts input about the target from the user and returns possible passwords in a .txt file. Password lists are custom made, based on the information the user provides. This caters for passwords that include native language words. pypass can be used when performing password cracking(ethically) using the dictionary attack.
+
+## usage
+
+git clone [project]
+create virtual environment
+run: pypass
 
 ### Be sure to leave a star on the project to show your support
 
--- pypass.py
+++ pypass.py
@@ -1,44 +1,57 @@
 #!/usr/bin/env python
-
+from datetime import datetime
 import click
-import datetime
 
-#function lower-cases the provided string
-#strips the string off all special characters and spaces
-def clean_input(a):
-    clean = a.lower()
+# function lower-cases the provided string
+# strips the string off all special characters and spaces
+
+
+def clean_input(text):
+    clean = text.lower()
     return ''.join(i for i in clean if i.isalnum())
 
-#function uses slicing to return a reverse of the string provided
-def reverser(a):
-    return a[::-1]
+# function uses slicing to return a reverse of the string provided
+
+
+def reverser(text):
+    return text[::-1]
+
+# function to convert datetime to string
+
+
+def convert_date(date_input):
+    date_string = date_input.strftime('%d%m%Y')
+    return date_string
+
 
 def takeother(other):
     return other.split(',')
 
-   
 
 @click.command()
 @click.option('--about', is_flag=True, help='Information about the program and its usage.')
-#@click.option('--full', is_flag=True, help='Option to get an almost exhaustive list of passwords. Requires more information about the target.')
-#interactive shell, accepting input from the user
+# @click.option('--full', is_flag=True, help='Option to get an almost exhaustive list of passwords.
+# Requires more information about the target.')
+# interactive shell, accepting input from the user
 def main(about):
 
     profile = {}
     pass_list = []
     if about:
-        click.echo("Welcome to pypass Version0.1!           pypass --help for more options")
+        click.echo(
+            "Welcome to pypass Version0.1!           pypass --help for more options")
     name = click.prompt('>>>Enter name ', type=str)
     surname = click.prompt('>>>Enter surname ', type=str)
     nickname = click.prompt('>>>Enter nickname ', type=str)
-    dob = click.prompt('>>>Enter date of birth(DDMMYYY) ', type=click.DateTime(formats=['%d%m%Y']))
+    dob = click.prompt('>>>Enter date of birth(DDMMYYY) ',
+                       type=click.DateTime(formats=['%d%m%Y']))
 
     profile['name'] = name
     profile['surname'] = surname
     profile['nickname'] = nickname
     profile['dob'] = dob
 
-    #print(profile)
+    # print(profile)
 
     pass0 = reverser(profile['name'])
     pass1 = profile['name'] + profile['surname']
@@ -47,17 +60,23 @@ def main(about):
     pass4 = reverser(profile['surname'])
     pass5 = profile['surname'] + profile['name']
     pass6 = profile['surname'] + profile['nickname']
+    pass7 = profile['name'] + convert_date(profile['dob'])
+    pass8 = profile['name'] + convert_date(profile['dob'])[:5]
+    pass9 = profile['name'] + convert_date(profile['dob'])[4:]
+    pass10 = profile['surname'] + convert_date(profile['dob'])
+    pass11 = profile['surname'] + convert_date(profile['dob'])[:5]
+    pass12 = profile['surname'] + convert_date(profile['dob'])[4:]
+    pass13 = profile['nickname'] + convert_date(profile['dob'])
+    pass14 = profile['nickname'] + convert_date(profile['dob'])[:5]
+    pass15 = profile['nickname'] + convert_date(profile['dob'])[4:]
 
-    pass_list = [pass0, pass1, pass2, pass3, pass4, pass5, pass6]
+    pass_list = [pass0, pass1, pass2, pass3,
+                 pass4, pass5, pass6, pass7, pass8, pass9, pass10, pass11, pass12, pass13, pass14, pass15]
 
-    for i in pass_list:
-        with open('passwords.txt', 'w+' ) as passfile:
-            passfile.write(i)
+    with open('passwords.txt', 'w+') as passfile:
+        for i in pass_list:
+            passfile.write("\n" + i)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
